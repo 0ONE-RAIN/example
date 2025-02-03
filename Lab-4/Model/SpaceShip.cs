426395431802e622900684abdadc96581fc99138
Daniel <daniel.pedraza@jala.university>
2025-02-02 19:13:43 -0500

    New commit

---
+++ Lab-4/Model/SpaceShip.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class SpaceShip
+{
+    public int PosicionY {  get;set; }
+
+   public SpaceShip() { 
+    
+        PosicionY = 0;
+    }
+
+    public void MoveUp()
+    {
+        PosicionY -= 10;
+
+    }
+
+    public void MoveDown() { PosicionY +=10; }
+}
---
+++ Lab-4/ViewModel/ShipViewModel.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+public class ShipViewModel : INotifyPropertyChanged
+{
+    private SpaceShip _shipModel;
+
+    public int PosicionY
+    {
+        get { return _shipModel.PosicionY; }
+        private set
+        {
+            if (_shipModel.PosicionY != value)
+            {
+                _shipModel.PosicionY = value;
+                OnPropertyChanged(nameof(PosicionY));
+            }
+        }
+    }
+
+    public ShipViewModel()
+    {
+        _shipModel = new SpaceShip();
+    }
+
+    public void MoveUp()
+    {
+        _shipModel.MoveUp();
+        OnPropertyChanged(nameof(PosicionY)); // Notificar que la posición cambió
+    }
+
+    public void MoveDown()
+    {
+        _shipModel.MoveDown();
+        OnPropertyChanged(nameof(PosicionY)); // Notificar que la posición cambió
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+    protected void OnPropertyChanged(string propertyName)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
